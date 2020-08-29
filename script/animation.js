857282fe2876a41265bdc5d58daaaf0ae4ef80d0
nolbo <52912534+nolbo@users.noreply.github.com>
2020-08-29 15:43:23 +0900

    Update animation.js

--- script/animation.js
+++ script/animation.js
@@ -1,9 +1,6 @@
 let isOpen = {"side" : false, "summary" : true, "api" : true, "basic" : true, "rhino" : true, "deepening" : true, "appendix" : true};
 
 $(function(){
-    if($("section#body").width == 0){
-        location.reload();
-    }
 
     $("div#blank").appendTo("div#sideBar");
     if($(window).width() > 800){
