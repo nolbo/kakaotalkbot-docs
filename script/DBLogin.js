21965c5f422f9c062c20d94fc0f8acbfff1bd6f4
franknoh <42113651+franknoh@users.noreply.github.com>
2020-09-30 14:34:31 +0900

    Delete DBLogin.js

--- script/DBLogin.js
+++
@@ -1,8 +0,0 @@
-firebase.auth().signInWithEmailAndPassword('db접근 이메일', 'db접근 비번').catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log('finish');
-    console.log('Error Code: ' + errorCode + '\nError Message: ' + errorMessage);
-    // ...
-});
