fa839b39b10b37aee830ce8ddb1a21ab19539b0d
franknoh <42113651+franknoh@users.noreply.github.com>
2020-10-01 14:51:09 +0900

    Update SignUp.js

--- script/SignUp.js
+++ script/SignUp.js
@@ -124,6 +124,7 @@ $(function(){
                     is = true;
                 }
                  // 비밀번호 검사
+		 /*
                  for(index in Upper){
                     if(pswd.includes(Upper[index]) == true){
                         isU = true;
@@ -132,6 +133,7 @@ $(function(){
                         isU = false;
                     }
                 }
+		*/
         
                 for(index in Lower){
                     if(pswd.includes(Lower[index]) == true){
@@ -159,7 +161,7 @@ $(function(){
                         isS = false;
                     }
                 }
-                if(isU == true && isL == true && isN == true && isS == true){
+                if((isU == true || =isL == true) && isN == true && isS == true){
                     $('#pswd').css('box-shadow', '0rem 0rem 0.9rem 0rem #b1b1b1');
                 }else{
                     $('#pswd').css('box-shadow', '0rem 0rem 0.9rem 0rem #ff0088');
