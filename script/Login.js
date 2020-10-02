fd2b25a4a894fb2c54e42a3f98c98935cc1764e2
franknoh <42113651+franknoh@users.noreply.github.com>
2020-10-02 15:24:41 +0900

    Update Login.js

--- script/Login.js
+++ script/Login.js
@@ -13,40 +13,41 @@ $(function() {
         event.preventDefault();
         firebase.database().ref().once("value").then(function(snapshot) {
             snapshot.forEach(function(childSnapshot) {
-                if (childSnapshot.val().id == $('#id').val()) {
+                if (childSnapshot.val().nickname == $('#id').val()) {
                     _email == childSnapshot.val().email
                 }
             })
         });
-        if(!!_email){
+        if (!!_email) {
             $('#id').css('box-shadow', '0rem 0rem 0.9rem 0rem #d1d1d1');
             $('#pswd').css('box-shadow', '0rem 0rem 0.9rem 0rem #ff0088');
             $('#id').val('');
             $('#pswd').val('');
         }
-        firebase.auth().signInWithEmailAndPassword(_email, $('#pswd').val()).catch(function(error) {
+        firebase.auth().signInWithEmailAndPassword(_email, $('#pswd').val()).then(function() {
+            var _id = GenerateHMAC('sha256', $('#id').val().replace(/ /g, ''));
+            var _pswd = GenerateHMAC('sha256', $('#pswd').val().replace(/ /g, ''));
+            var DB = firebase.database().ref(firebase.auth().currentUser.uid);
+
+
+
+            DB.once("value").then(function(snapshot) {
+                var user = snapshot.val();
+
+                if (user == undefined) { //id가 없을때
+                    $('#pswd').css('box-shadow', '0rem 0rem 0.9rem 0rem #d1d1d1');
+                    $('#id').css('box-shadow', '0rem 0rem 0.9rem 0rem #ff0088');
+                    $('#id').val('');
+                    $('#pswd').val('');
+                } else {
+                    location.href = 'https://kkotbot-docs.kro.kr/'; //로그인 후 이동
+                }
+            });
+        }).catch(function(error) {
             $('#id').css('box-shadow', '0rem 0rem 0.9rem 0rem #d1d1d1');
             $('#pswd').css('box-shadow', '0rem 0rem 0.9rem 0rem #ff0088');
             $('#id').val('');
             $('#pswd').val('');
-        });
-        var _id = GenerateHMAC('sha256', $('#id').val().replace(/ /g, ''));
-        var _pswd = GenerateHMAC('sha256', $('#pswd').val().replace(/ /g, ''));
-        var DB = firebase.database().ref(firebase.auth().currentUser.uid);
-
-
-
-        DB.once("value").then(function(snapshot) {
-            var user = snapshot.val();
-
-            if (user == undefined) { //id가 없을때
-                $('#pswd').css('box-shadow', '0rem 0rem 0.9rem 0rem #d1d1d1');
-                $('#id').css('box-shadow', '0rem 0rem 0.9rem 0rem #ff0088');
-                $('#id').val('');
-                $('#pswd').val('');
-            } else {
-                location.href = 'https://kkotbot-docs.kro.kr/'; //로그인 후 이동
-            }
         });
     });
 });
