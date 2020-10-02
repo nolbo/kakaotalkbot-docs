d7509e394ac49de73047e56ec0c68f6a59f94cb8
franknoh <42113651+franknoh@users.noreply.github.com>
2020-10-02 20:12:52 +0900

    Update Login.js

--- script/Login.js
+++ script/Login.js
@@ -17,7 +17,11 @@ function login() {
             } else {
                 console.log(_email)
                 firebase.auth().signInWithEmailAndPassword(_email, $('#pswd').val()).then(function() {
-                    location.href = 'https://kkotbot-docs.kro.kr/'; //로그인 후 이동
+                    if (location.href.startsWith('https://kkotbot-docs.kro.kr/login?redirect=')) {
+                        location.href = location.href.substring('https://kkotbot-docs.kro.kr/login?redirect='.length); //로그인 후 이동
+                    } else {
+                        location.href = 'https://kkotbot-docs.kro.kr/'; //로그인 후 이동
+                    }
                 }).catch(function(error) {
                     console.log(error);
                     $('#id').css('box-shadow', '0rem 0rem 0.9rem 0rem #d1d1d1');
