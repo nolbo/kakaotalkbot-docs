fcaabace63b0b3a78aeba7569ed1c9a36ddbe3f7
franknoh <42113651+franknoh@users.noreply.github.com>
2020-10-02 15:58:03 +0900

    Update Login.js

--- script/Login.js
+++ script/Login.js
@@ -6,48 +6,50 @@ function GenerateHMAC(key, payload) { //암호화 함수
 }
 
 $(function() {
+    var _email;
     if (!!firebase.auth().currentUser) {
         location.href = 'https://kkotbot-docs.kro.kr/';
-    }
-    $('button#complete').click(function(event) {
-        event.preventDefault();
-        firebase.database().ref().once("value").then(function(snapshot) {
-            snapshot.forEach(function(childSnapshot) {
-                if (childSnapshot.val().nickname == $('#id').val()) {
-                    _email == childSnapshot.val().email
-                }
-            })
-        });
-        if (!!_email) {
-            $('#id').css('box-shadow', '0rem 0rem 0.9rem 0rem #d1d1d1');
-            $('#pswd').css('box-shadow', '0rem 0rem 0.9rem 0rem #ff0088');
-            $('#id').val('');
-            $('#pswd').val('');
-        }
-        firebase.auth().signInWithEmailAndPassword(_email, $('#pswd').val()).then(function() {
-            var _id = GenerateHMAC('sha256', $('#id').val().replace(/ /g, ''));
-            var _pswd = GenerateHMAC('sha256', $('#pswd').val().replace(/ /g, ''));
-            var DB = firebase.database().ref(firebase.auth().currentUser.uid);
-
-
-
-            DB.once("value").then(function(snapshot) {
-                var user = snapshot.val();
-
-                if (user == undefined) { //id가 없을때
-                    $('#pswd').css('box-shadow', '0rem 0rem 0.9rem 0rem #d1d1d1');
-                    $('#id').css('box-shadow', '0rem 0rem 0.9rem 0rem #ff0088');
-                    $('#id').val('');
-                    $('#pswd').val('');
-                } else {
-                    location.href = 'https://kkotbot-docs.kro.kr/'; //로그인 후 이동
-                }
+    } else {
+        $('button#complete').click(function(event) {
+            event.preventDefault();
+            firebase.database().ref().once("value").then(function(snapshot) {
+                snapshot.forEach(function(childSnapshot) {
+                    if (childSnapshot.val().nickname == $('#id').val()) {
+                        _email == childSnapshot.val().email
+                    }
+                })
             });
-        }).catch(function(error) {
-            $('#id').css('box-shadow', '0rem 0rem 0.9rem 0rem #d1d1d1');
-            $('#pswd').css('box-shadow', '0rem 0rem 0.9rem 0rem #ff0088');
-            $('#id').val('');
-            $('#pswd').val('');
+            if (!!_email) {
+                $('#id').css('box-shadow', '0rem 0rem 0.9rem 0rem #d1d1d1');
+                $('#pswd').css('box-shadow', '0rem 0rem 0.9rem 0rem #ff0088');
+                $('#id').val('');
+                $('#pswd').val('');
+            }
+            firebase.auth().signInWithEmailAndPassword(_email, $('#pswd').val()).then(function() {
+                var _id = GenerateHMAC('sha256', $('#id').val().replace(/ /g, ''));
+                var _pswd = GenerateHMAC('sha256', $('#pswd').val().replace(/ /g, ''));
+                var DB = firebase.database().ref(firebase.auth().currentUser.uid);
+
+
+
+                DB.once("value").then(function(snapshot) {
+                    var user = snapshot.val();
+
+                    if (user == undefined) { //id가 없을때
+                        $('#pswd').css('box-shadow', '0rem 0rem 0.9rem 0rem #d1d1d1');
+                        $('#id').css('box-shadow', '0rem 0rem 0.9rem 0rem #ff0088');
+                        $('#id').val('');
+                        $('#pswd').val('');
+                    } else {
+                        location.href = 'https://kkotbot-docs.kro.kr/'; //로그인 후 이동
+                    }
+                });
+            }).catch(function(error) {
+                $('#id').css('box-shadow', '0rem 0rem 0.9rem 0rem #d1d1d1');
+                $('#pswd').css('box-shadow', '0rem 0rem 0.9rem 0rem #ff0088');
+                $('#id').val('');
+                $('#pswd').val('');
+            });
         });
-    });
+    }
 });
