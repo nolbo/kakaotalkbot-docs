3186126dbce3ea5bec6e276cd891f9ef5335e1b9
franknoh <42113651+franknoh@users.noreply.github.com>
2020-12-06 18:53:22 +0900

    0

---
+++ node/grp.js
@@ -0,0 +1,20 @@
+var fs = require('fs'),
+    fileList = ['./index.html', './summary.html', './rhino.html', './legacyAPI.html', './full.html', './deepening.html', './API2.html', './appendix.html'],
+    path = 'img/grp/default.jpg',
+    date = ((new Date()).getMonth() + 1) + '/' + (new Date()).getDate();
+if (date == '12/25') {
+    path = 'img/grp/christmas.jpg';
+} else if ([].includes(date)) {
+    path = 'img/grp/grp_holid.jpg';
+} else if ([].includes(date)) {
+    path = 'img/grp/grp_ko.jpg';
+}
+fileList.forEach((e) => {
+    fs.readFile(e, function (err, data) {
+        if (err) throw err;
+        data = data.toString().split('\n');
+        data[13] = '<meta property="og:image" content="' + path + '">';
+        data = data.join('\n');
+        fs.writeFile(e, data, function (err) { err || console.log('Data replaced \n', data); });
+    });
+});
--- node/grp/default.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs'),
-fileList = ['/index.html', '/summary.html', '/rhino.html', '/legacyAPI.html', '/full.html', '/deepening.html', '/API2.html', '/appendix.html'];
-fileList.forEach((e)=>{
-fs.readFile(e, function(err, data) { if(err) throw err;
- data = data.toString().split('\n');
- data[13] = '<meta property="og:image" content="img/grp/default.jpg">';
-data=data.join('\n');
- fs.writeFile(e, data, function(err) { err || console.log('Data replaced \n', data); }); });
-});
