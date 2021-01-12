707907cb207f404b6e37b4317b2277ae051eda29
franknoh <42113651+franknoh@users.noreply.github.com>
2021-01-12 10:43:38 +0900

    Update signUp.js

--- script/signUp.js
+++ script/signUp.js
@@ -24,75 +24,76 @@ $(function () {
         var email = $('#email').val().replace(/ /g, '');
         var is = false;
         var pwVal = con(Upper, pswd) && con(Lower, pswd) && con(Nums, pswd) && con(Symbols, pswd);
-
-        if (id.length === 0) {
-            $('#email').removeAttr('style');
-            $('#id').css('box-shadow', '0rem 0rem 0.8rem 0rem #ff0088');
-            setTimeout(() => {
-                $('#id').val('');
-            }, 100);
-            is = true;
-        } else if (email.length === 0 || !email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
-            $('#id').removeAttr('style');
-            $('#email').css('box-shadow', '0rem 0rem 0.8rem 0rem #ff0088');
-            $('#email').val('');
-            is = true;
-        }
-
-
-        Symbols.forEach((e) => {
-            if (id.indexOf(e) != -1) {
+        setTimeout(() => {
+            if (id.length === 0) {
                 $('#email').removeAttr('style');
                 $('#id').css('box-shadow', '0rem 0rem 0.8rem 0rem #ff0088');
-                $('#id').val('');
+                setTimeout(() => {
+                    $('#id').val('');
+                }, 100);
+                is = true;
+            } else if (email.length === 0 || !email.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
+                $('#id').removeAttr('style');
+                $('#email').css('box-shadow', '0rem 0rem 0.8rem 0rem #ff0088');
+                $('#email').val('');
                 is = true;
             }
-        });
 
-        if (pwVal) {
-            $('#password').removeAttr('style');
-        } else {
-            console.log(con(Upper, pswd));
-            console.log(con(Lower, pswd));
-            console.log(con(Nums, pswd));
-            console.log(con(Symbols, pswd));
-            $('#password').css('box-shadow', '0rem 0rem 0.8rem 0rem #ff0088');
-            $('#password').val('');
-            is = true;
-        }
-        // 비밀번호 확인 검사
-        if (repswd == pswd) {
-            $('#re_password').removeAttr('style');
-        } else {
-            $('#re-password').css('box-shadow', '0rem 0rem 0.8rem 0rem #ff0088');
-            $('#re-password').val('');
-            is = true;
-        }
 
-        if (!($('input#privacy').prop('checked') && $('input#location').prop('checked'))) {
-            is = true;
-        }
+            Symbols.forEach((e) => {
+                if (id.indexOf(e) != -1) {
+                    $('#email').removeAttr('style');
+                    $('#id').css('box-shadow', '0rem 0rem 0.8rem 0rem #ff0088');
+                    $('#id').val('');
+                    is = true;
+                }
+            });
 
-        if (!is) {
-            console.log('create account');
-            newA = true;
-            var http = new XMLHttpRequest();
-            var url = 'https://kbot-auth.herokuapp.com/user/create';
-            var params = 'id=' + encodeURIComponent(id) + '&email=' + encodeURIComponent(email) + '&pw=' + encodeURIComponent(pswd) + '&profile=' + encodeURIComponent($('img#profile').attr('src'));
-            http.open('POST', url, true);
-            http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            if (pwVal) {
+                $('#password').removeAttr('style');
+            } else {
+                console.log(con(Upper, pswd));
+                console.log(con(Lower, pswd));
+                console.log(con(Nums, pswd));
+                console.log(con(Symbols, pswd));
+                $('#password').css('box-shadow', '0rem 0rem 0.8rem 0rem #ff0088');
+                $('#password').val('');
+                is = true;
+            }
+            // 비밀번호 확인 검사
+            if (repswd == pswd) {
+                $('#re_password').removeAttr('style');
+            } else {
+                $('#re-password').css('box-shadow', '0rem 0rem 0.8rem 0rem #ff0088');
+                $('#re-password').val('');
+                is = true;
+            }
 
-            http.onreadystatechange = function () {
-                if (http.readyState == 4) {
-                    if (http.status == 200) {
-                        location.href = location.origin + '/signin.html';
-                    } else if (http.status == 400) {
-                        newA = false;
+            if (!($('input#privacy').prop('checked') && $('input#location').prop('checked'))) {
+                is = true;
+            }
+
+            if (!is) {
+                console.log('create account');
+                newA = true;
+                var http = new XMLHttpRequest();
+                var url = 'https://kbot-auth.herokuapp.com/user/create';
+                var params = 'id=' + encodeURIComponent(id) + '&email=' + encodeURIComponent(email) + '&pw=' + encodeURIComponent(pswd) + '&profile=' + encodeURIComponent($('img#profile').attr('src'));
+                http.open('POST', url, true);
+                http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+                http.onreadystatechange = function () {
+                    if (http.readyState == 4) {
+                        if (http.status == 200) {
+                            location.href = location.origin + '/signin.html';
+                        } else if (http.status == 400) {
+                            newA = false;
+                        }
                     }
                 }
+                http.send(params);
             }
-            http.send(params);
-        }
+        }, 100)
     }
 
     $('#complete').click(function (event) {
